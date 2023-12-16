e42adf32d8002e273758188f6ec7adf7c3ed830a
Franco Di Leo <dileo.francoj@gmail.com>
2023-12-16 17:55:27 -0300

    feat: initialize test config and add husky config

---
+++ src/check/__tests__/isEmpty.test.ts
@@ -0,0 +1,28 @@
+import { isEmpty } from '..'
+
+describe('isEmpty', () => {
+  test('Should return false for a no empty array', () => {
+    const element = ['first item']
+    expect(isEmpty(element)).toBe(false)
+  })
+
+  test('Should return false for a non empty object', () => {
+    const element = { name: 'john' }
+    expect(isEmpty(element)).toBe(false)
+  })
+
+  test('Should return false for an empty matrix', () => {
+    const element = [[]]
+    expect(isEmpty(element)).toBe(false)
+  })
+
+  test('Should return true for an empty array', () => {
+    const element: unknown = []
+    expect(isEmpty(element)).toBeTruthy()
+  })
+
+  test('Should return true for an empty object', () => {
+    const element: unknown = {}
+    expect(isEmpty(element)).toBeTruthy()
+  })
+})
