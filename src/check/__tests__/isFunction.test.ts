67cc3c63dffb99c5bf70380737a55484c8d71ad5
Franco Di Leo <dileo.francoj@gmail.com>
2023-12-16 19:50:21 -0300

    test: add isFunction test

---
+++ src/check/__tests__/isFunction.test.ts
@@ -0,0 +1,18 @@
+import { isFunction } from '..'
+
+describe('isFunction', () => {
+  test.each([
+    ['some string', false],
+    [4, false],
+    [true, false]
+  ])('should return falsy if the prop is not a function', (element, expectedResult) => {
+    expect(isFunction(element)).toBe(expectedResult)
+  })
+
+  test.each([
+    [() => {}],
+    [function () {}]
+  ])('should return true if the prop is a function', (element) => {
+    expect(isFunction(element)).toBe(true)
+  })
+})
