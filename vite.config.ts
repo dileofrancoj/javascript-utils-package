a1e300000d1441ae62e396f31a298fc6bb9ee920
Franco Di Leo <dileo.francoj@gmail.com>
2023-12-16 16:09:58 -0300

    chore: add vite aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import path from 'path/posix';
 import dts from "vite-plugin-dts";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   build: {
@@ -10,5 +12,10 @@ export default defineConfig({
       fileName: "my-lib",
     },
   },
-  plugins: [dts()],
+  plugins: [dts(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, './src/*')
+    }
+  }
 });
